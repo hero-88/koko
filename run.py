0a6f1772d66479db005b44c0e638f4c295de156d
hero-88 <108606711+hero-88@users.noreply.github.com>
2022-11-07 00:58:09 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("main").menu()
+       __import__("hero").menu()
    except Exception as e:
        exit(str(e))
